e2fc85c8cd17630ebc6121b9c453e0586c774fe7
tad1 <tadziobrzeski@gmail.com>
2023-11-17 16:31:29 +0000

    update on tutorial 02

---
+++ src/_arch/aarch64/cpu/boot.s
@@ -0,0 +1,39 @@
+
+
+.macro ADR_REL register, symbol
+    adrp \register, \symbol
+    add \register, \register, #:lo12:\symbol
+.endm
+
+.section .text._start
+
+_start:
+    mrs	x0, MPIDR_EL1
+    and x0, x0, {CONST_CORE_ID_MASK}
+    ldr x1, BOOT_CORE_ID
+    cmp x0, x1
+    b.ne .L_parking_loop
+
+    ADR_REL x0, __bss_start
+    ADR_REL x1, __bss_end_exclusive
+
+.L_bss_init_loop:
+    cmp x0, x1
+    b.eq .L_prepare_rust
+    stp xzr, xzr, [x0], #16
+    b .L_bss_init_loop
+
+.L_prepare_rust:
+    // set stack pointer
+    ADR_REL x0, __boot_core_stack_end_exclusive
+    mov sp, x0
+
+    b _start_rust
+
+.L_parking_loop:
+    wfe
+    b .L_parking_loop
+
+.size _start, . -_start
+.type _start function
+.global _start
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,11 @@
+#![feature(asm_const)]
 #![no_main]
 #![no_std]
 
-use core::{panic::PanicInfo, arch::asm};
-use aarch64_cpu::{asm, registers};
+mod bsp;
+mod cpu;
+mod panic_wait;
 
-
-#[no_mangle]
-pub unsafe extern "C" fn Init() -> ! {
-    asm!(include_str!("single_core_init.s"));
-    Loop();
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn Loop() -> ! {
-    let mut _x = 42;
-    
-    asm!("wfi"); // halts CPU
-    loop {}
-}
-
-#[panic_handler]
-fn panic(_panic: &PanicInfo<'_>) -> ! {
-    loop {}
+unsafe fn kernel_init() -> !{
+    panic!()
 }
